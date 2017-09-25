53bb23b3325f0bd28a881c902470bd12b716cbd5
Makar Kalancha <makarkalancha@gmail.com>
2017-09-25 09:17:23 -0400

    autowire annotation

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,11 +2,15 @@ package com.pluralsight.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
+	
+	@Autowired
 	private CustomerRepository customerRepository;
 	
 	
